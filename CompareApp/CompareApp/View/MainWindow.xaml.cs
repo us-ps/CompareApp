a959e8c8f4c5fd0b664b951bee55e1b001d5ba73
Utkrisht Sharma <utkrisht_sharma@persistent.com>
2023-03-06 10:24:26 +0530

    upload button added

---
+++ CompareApp/CompareApp/View/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using CompareApp.Model;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompareApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UploadButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Files (*.txt)|*.txt";
+            bool? @bool = ofd.ShowDialog();
+
+            if (@bool == true)
+            {
+                //_textFile = new MainModel
+                //{
+                //    FilePath = ofd.FileName,
+                //    FileContent = File.ReadAllText(ofd.FileName)
+                //};
+                MessageBox.Show(ofd.FileName + File.ReadAllText(ofd.FileName));
+            }
+        }
+    }
+}
---
+++ CompareApp/CompareApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CompareApp.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareApp.ViewModel
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private MainModel _model;
+
+        public string Content
+        {
+            get { return _model.FileContent; }
+            set 
+            {
+                _model.FileContent = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Content)));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
